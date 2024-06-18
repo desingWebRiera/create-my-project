eb07b02b2091652800d4fe5d5990ff96939e1474
desingWebRiera <desingWebRiera@gmail.com>
2024-06-18 13:20:07 +0200

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hola Mundo!!
+    <!--mis componentes-->
+    <div id="boton">
+      <Boton/>
+
+    </div>
+  </h1>
+
+</template>
+  <script setup>
+  import Boton from "@/components/Boton.vue";
+  export default {
+    name: "Boton",
+    components: {
+      Boton
+    }
+  };
+  
+  </script>
+
+<style scoped>
+
+</style>
---
+++ src/Boton.vue
@@ -0,0 +1,10 @@
+<template>
+<input type="button" class="text" id="boton">Agradecimientos</input>
+</template>
+<script>
+
+
+</script>
+<style>
+
+</style>
